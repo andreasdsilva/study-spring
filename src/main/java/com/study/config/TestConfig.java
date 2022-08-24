90e462ad54bf0d06f590b837a68b50b0bb28de9e
Andreas <ads@interact.com.br>
2022-08-24 11:56:58 -0300

    Feat; Category class, service, resource

--- src/main/java/com/study/config/TestConfig.java
+++ src/main/java/com/study/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.study.entities.Category;
 import com.study.entities.Order;
 import com.study.entities.User;
 import com.study.entities.enums.OrderStatus;
+import com.study.repositories.CategoryRepository;
 import com.study.repositories.OrderRepository;
 import com.study.repositories.UserRepository;
 
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -37,6 +42,12 @@ public class TestConfig implements CommandLineRunner{
 		Order o3 = new Order( null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT );
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 	
 	
