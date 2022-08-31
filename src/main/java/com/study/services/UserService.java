8586b408a175b8db104908b9edf398c6dc79cde4
Andreas <ads@interact.com.br>
2022-08-31 11:51:47 -0300

    Feat: get exception handler, notFound

--- src/main/java/com/study/services/UserService.java
+++ src/main/java/com/study/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.study.entities.User;
 import com.study.repositories.UserRepository;
+import com.study.services.Exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -23,7 +24,7 @@ public class UserService {
 	public User findById( Long id )
 	{
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert( User obj ) {
